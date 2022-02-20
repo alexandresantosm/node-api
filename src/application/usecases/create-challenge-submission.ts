772f080fcae4a3c89e0f4df7e70b6790570b48b7
alexandresantosm <amenezes031@gmail.com>
2022-02-20 12:45:03 -0300

    inject StudentsRepository class in the constructor CreateChallengeSubmission class

--- src/application/usecases/create-challenge-submission.ts
+++ src/application/usecases/create-challenge-submission.ts
@@ -1,4 +1,5 @@
 import { Submission } from "../../domain/entities/submission";
+import { StudentsRepository } from "../repositories/StudentsRepository";
 
 type CreateChallengeSubmissionRequest = {
   studentId: string;
@@ -6,10 +7,18 @@ type CreateChallengeSubmissionRequest = {
 };
 
 export class CreateChallengeSubmission {
+  constructor(private readonly studentsRepository: StudentsRepository) {}
+
   execute = async ({
     studentId,
     challengeId,
   }: CreateChallengeSubmissionRequest) => {
+    const student = await this.studentsRepository.findById(studentId);
+
+    if (!student) {
+      throw new Error("Students does not exists.");
+    }
+
     const submission = Submission.create({
       studentId,
       challengeId,
